6c25b6ae733db36972dc7c04285072c9b35f5ad4
Ashan Rajapaksha <rajapaksha.live@gmail.com>
2023-10-31 08:21:37 +0530

    tests updated

--- test/coordinate.controller.spec.ts
+++ test/coordinate.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoordinatesController } from '../src/coordinates/coordinates.controller';
 import { CoordinatesService } from '../src/coordinates/coordinates.service'; // Import the service
+import { getModelToken } from '@nestjs/mongoose'; 
 
 describe('CoordinatesController', () => {
   let controller: CoordinatesController;
@@ -8,7 +9,13 @@ describe('CoordinatesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoordinatesController],
-      providers: [CoordinatesService], // Add the service here
+      providers: [
+        CoordinatesService, // Add the service here
+        {
+          provide: getModelToken('Coordinate'), // Mock the model
+          useValue: {}, // Provide a mock implementation
+        },
+      ],
       // imports: [SomeOtherModule], // If needed, import other modules
     }).compile();
 
