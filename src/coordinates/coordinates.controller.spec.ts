2421c034b4f0727738f79a005135b87f57a91129
Ashan Rajapaksha <rajapaksha.live@gmail.com>
2023-10-31 09:43:38 +0530

    kafka tests updated

--- src/coordinates/coordinates.controller.spec.ts
+++ src/coordinates/coordinates.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoordinatesController } from './coordinates.controller';
+import { CoordinatesService } from './coordinates.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('CoordinatesController', () => {
   let controller: CoordinatesController;
@@ -7,6 +9,13 @@ describe('CoordinatesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoordinatesController],
+      providers: [
+        CoordinatesService,
+        {
+          provide: getModelToken('Coordinate'),
+          useValue: {}, // Mock the Mongoose model here
+        },
+      ],
     }).compile();
 
     controller = module.get<CoordinatesController>(CoordinatesController);
