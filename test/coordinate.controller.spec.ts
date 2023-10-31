c973249c80adb43fe2a50079edcadc0378398646
Ashan Rajapaksha <rajapaksha.live@gmail.com>
2023-10-31 09:35:20 +0530

    kafka config updated

--- test/coordinate.controller.spec.ts
+++ test/coordinate.controller.spec.ts
@@ -4,25 +4,24 @@ import { CoordinatesService } from '../src/coordinates/coordinates.service'; // 
 import { getModelToken } from '@nestjs/mongoose'; 
 
 describe('CoordinatesController', () => {
-  let controller: CoordinatesController;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CoordinatesController],
-      providers: [
-        CoordinatesService, // Add the service here
-        {
-          provide: getModelToken('Coordinate'), // Mock the model
-          useValue: {}, // Provide a mock implementation
-        },
-      ],
-      // imports: [SomeOtherModule], // If needed, import other modules
-    }).compile();
-
-    controller = module.get<CoordinatesController>(CoordinatesController);
+    let controller: CoordinatesController;
+  
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [CoordinatesController],
+        providers: [
+          CoordinatesService,
+          {
+            provide: getModelToken('Coordinate'),
+            useValue: {}, // Mock the Mongoose model here
+          },
+        ],
+      }).compile();
+  
+      controller = module.get<CoordinatesController>(CoordinatesController);
+    });
+  
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
   });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
