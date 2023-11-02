8becfa1cdbfe24406a2cab5fd9502fa9c202c63f
Ashan Rajapaksha <rajapaksha.live@gmail.com>
2023-11-02 08:35:04 +0530

    updated swagger for routes

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Get, Body } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 
@@ -8,11 +8,13 @@ import { CreateRouteDto } from './dto/create-route.dto';
 export class RoutesController {
   constructor(private readonly routesService: RoutesService) {}
 
+  @ApiOperation({ summary: 'Create a new route' })
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
     return this.routesService.create(createRouteDto);
   }
 
+  @ApiOperation({ summary: 'Get existing routes' })
   @Get()
   findAll() {
     return this.routesService.findAll();
